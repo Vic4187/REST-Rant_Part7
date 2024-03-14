ac56bea9f5bd0541b0449d6296e084faca1f880f
Victor Estrada <Vic4187@gmail.com>
2024-03-13 22:56:38 -0500

    Initial commit

---
+++ controllers/places.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const places = require('../models/places')
+
+// GET /places
+router.get('/', (req, res) => {  
+    res.render('places/index', { places})
+  })
+
+  router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+  router.get('/', (req, res) => {
+    res.send('GET /places')
+  })
+
+  router.post('/', (req, res) => {
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+module.exports = router
---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+app.use('/places', require('./controllers/places'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('*', (req, res) => {
+  res.render('error404')
+})
+
+app.listen(process.env.PORT)
